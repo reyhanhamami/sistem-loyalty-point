6d28802be5a3db29bfd78152fbc3b841e39bb9a8
reyhanhamami <reyhanhamami@gmail.com>
2019-07-02 19:39:54 +0700

    fix total transaksi

--- loyalty_point/loyalty_point/doctype/transaksi/transaksi.js
+++ loyalty_point/loyalty_point/doctype/transaksi/transaksi.js
@@ -20,21 +20,17 @@ frappe.ui.form.on("Transaksi Line", {
 			callback: function(data) {
 				if (data) {
 					// menghitung total harga dan point
-					// var jumlah_tukar = data.message.best_point * row.jumlah_item
 					var harga_akhir = data.message.harga_item * row.jumlah_item
-					// var point_yang_tukar = 0.001 * harga_akhir
 
-					// frappe.model.set_value(row.doctype, row.name, 'point_tukar', jumlah_tukar)
 					frappe.model.set_value(row.doctype, row.name, 'harga_item', harga_akhir)
-					// frappe.model.set_value(row.doctype, row.name, 'point_peroleh', point_yang_tukar)
 
-					// // alert jika stok kurang
-					// if (row.jumlah_item >= data.message.jumlah_item) {
-					// 	msgprint('Stok tidak mencukupi.')
-					// 	frappe.model.set_value(row.doctype, row.name, 'jumlah_item', data.message.jumlah_item)
+					// // alert jika duit kurang
+					// if (row.saldo_member >= data.message.saldo_member) {
+					// 	msgprint('Uang tidak mencukupi.')
+					// 	frappe.model.set_value(row.doctype, row.name, 'saldo_member', data.message.saldo_member)
 					// }
 				}
-				refresh_field('jumlah_item')
+				// refresh_field('saldo_member')
 			}
 		})
 	},
@@ -49,29 +45,23 @@ frappe.ui.form.on("Transaksi Line", {
 			total_transaksi += row.harga_item
 			// total_point += row.point_peroleh
 		})
-	
-		// console.log(total_transaksi)
-		frm.set_value('total_transaksi', total_transaksi)
-		// frm.set_value('calculate', total_point)
 
+		frm.set_value('total_transaksi', total_transaksi)
 		refresh_field('total_transaksi')
-		// refresh_field('calculate')
 	},
+	
 	// mengurangi total transaksi sesuai pada harga jumlah item yang dihapus
 	item_remove:function(frm, cdt, cdn){
 		var row = locals[cdt][cdn]
 		
 		var total_transaksi = 0
-		// var total_point = 0
 		
 		frm.doc.item.forEach(function(row) { 
 			total_transaksi += row.harga_item
-			// total_point += row.point_peroleh
+		
 		})
-		// console.log(total_transaksi)
+
 		frm.set_value('total_transaksi', total_transaksi)
-		// // frm.set_value('calculate', total_point)
 		refresh_field('total_transaksi')
-		// // refresh_field('calculate')
 	}
 });
